5561830a286d9a2dab4aafd677d92c1aa8afaa78
Мирлан Садамбеков <sadambekovmirlan9@gmail.com>
2023-03-21 21:38:25 +0600

    homework3

--- products/models.py
+++ products/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Products(models.Model):
-    image = models.ImageField(blank=True , null= True)
+    image = models.ImageField(blank=True, null=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField(default=0)
     rate = models.FloatField(default=0.0)
     created_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Products
+from products.models import Products ,Comment
 
 
 # Create your views here.
@@ -7,12 +7,24 @@ def main_page_view(request):
     if request.method == 'GET':
         return render(request, 'layouts/index.html')
 
+
 def products_view(request):
     if request.method == 'GET':
-
-        products =Products.objects.all()
+        products = Products.objects.all()
 
         context = {
-            'products':products
+            'products': products
         }
         return render(request, 'products/products.html', context=context)
+
+
+def product_detail_view(request, id):
+    if request.method == 'GET':
+        product = Products.objects.get(id=id)
+
+        context = {
+            "product":product,
+            "comments" :product.comment_set.all()
+        }
+
+        return render(request , 'products/detail.html' ,context=context)
