1650d38b98433aafa683a7fd20f08fd26c532f31
Мирлан Садамбеков <sadambekovmirlan9@gmail.com>
2023-03-23 17:16:56 +0600

    homework3

--- Shop4m/urls.py
+++ Shop4m/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from products.views import main_page_view , products_view , product_detail_view
-
-
+from Shop4m.settings import MEDIA_URL, MEDIA_ROOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',main_page_view ),
     path('products/', products_view),
     path('products/<int:id>/', product_detail_view)
 ]
+urlpatterns+= static(MEDIA_URL, document_root = MEDIA_ROOT)
