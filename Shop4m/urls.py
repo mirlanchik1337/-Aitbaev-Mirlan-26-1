e8f8b8570fc8f3ecb016c852928dada5abe39e0e
Мирлан Садамбеков <sadambekovmirlan9@gmail.com>
2023-04-02 23:30:22 +0600

    formsregister

--- Shop4m/urls.py
+++ Shop4m/urls.py
@@ -19,12 +19,14 @@ from django.urls import path
 from products.views import main_page_view, products_view, product_detail_view, create_product_view
 from Shop4m.settings import MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
+from users.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page_view),
     path('products/', products_view),
     path('products/<int:id>/', product_detail_view),
-    path('products/create/', create_product_view)
-]
-urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+    path('products/create/', create_product_view),
+
+    path('users/register/', register_view)
+]+ static(MEDIA_URL, document_root=MEDIA_ROOT)
